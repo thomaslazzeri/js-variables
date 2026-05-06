42dbc9ca06e93eabc12ec401c87c23715a6903c4
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-06 20:51:44 +0200

    Fix calculations for numThreeMinusNumOne and sum in main.js

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ const numThreeDividedByNumOne = numThree / numOne;
 console.log(numThreeDividedByNumOne); // Output: 4
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = NaN;
+const numThreeMinusNumOne = numThree - numOne;
 console.log(numThreeMinusNumOne); // Output: 24
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = NaN;
+const sum = numOne + numTwo + numThree;
 console.log(sum); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
