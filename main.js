bfdc308cae581fab71619190c504fe793d6f8d61
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-06 21:17:43 +0200

    done exercise number 2: number, text and bool

--- main.js
+++ main.js
@@ -40,7 +40,12 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+const number = 1;
+console.log(number);
+const text = 'ciao';
+console.log(text);
+const bool = 5 < 5000;
+console.log(bool);
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
